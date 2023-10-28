f3d35369d4a5789c99b74c048fc7ceda83e17d1e
AhmadMuzayyin <muzayyin654@gmail.com>
2023-10-28 17:53:49 +0700

    add export transaction finance feature

---
+++ app/Http/Controllers/Sinkron/SinkronController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers\Sinkron;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Revolution\Google\Sheets\Facades\Sheets;
+
+class SinkronController extends Controller
+{
+    public function show()
+    {
+        $sheet_name = env('SHEET_NAME');
+        $sheet_id = env('SPREDSHEET_ID');
+//        $values = Sheets::spreadsheet('YOUR_SPREADSHEET_ID')->sheet('Test')->get();
+        $row = [7, 'ahmad7@gmail.com', 'ahmad7'];
+        Sheets::spreadsheet('YOUR_SPREADSHEET_ID')->sheet('Test')->append([$row]);
+        $values = Sheets::all();
+        dd($values);
+    }
+}
